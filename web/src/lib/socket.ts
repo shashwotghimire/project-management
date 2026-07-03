b1169edd657967eb0bd8a21252c88847e6db5a60
shashwotghimire <shashwot234@gmail.com>
2026-07-03 23:42:42 +0545

    fix docker compose

--- web/src/lib/socket.ts
+++ web/src/lib/socket.ts
@@ -1,10 +1,10 @@
 import { io, Socket } from "socket.io-client";
 
 let socket: Socket | null = null;
-
+console.log("SOCKET_URL:", process.env.NEXT_PUBLIC_API_URL);
 export const getSocket = (): Socket => {
   if (!socket) {
-    socket = io(process.env.NEXT_PUBLIC_API_URL!.replace("/api", ""), {
+    socket = io(process.env.NEXT_PUBLIC_SOCKET_URL, {
       autoConnect: false,
     });
   }
